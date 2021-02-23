d859ceb1292345f1e3d175b579809a95d6d0383b
Patel <patelsd@ad.msoe.edu>
2021-02-23 09:28:03 -0600

    Player animations complete

--- game.py
+++ game.py
@@ -3,6 +3,9 @@ from pygame.locals import *
 
 pygame.init()
 
+clock = pygame.time.Clock()
+fps = 60
+
 screen_width = 1000
 screen_height = 1000
 
@@ -12,7 +15,6 @@ pygame.display.set_caption('Platformer')
 # Define game variables
 tile_size = 50
 
-
 # Load images
 bg_img = pygame.transform.scale(pygame.image.load('img/sky.png'), (screen_width, screen_height))
 
@@ -21,6 +23,12 @@ def draw_grid():
     for line in range(0, 20):
         pygame.draw.line(screen, (255, 255, 255), (0, line * tile_size), (screen_width, line * tile_size))
         pygame.draw.line(screen, (255, 255, 255), (line * tile_size, 0), (line * tile_size, screen_height))
+
+
+def standing(self):
+    self.counter = 0
+    self.index = 0
+    self.image = self.images_right[self.index] if (self.dir_facing == 1) else self.images_left[self.index]
 
 
 class World:
@@ -55,6 +63,83 @@ class World:
             screen.blit(tile[0], tile[1])
 
 
+class Player:
+    def __init__(self, x, y):
+        self.images_right, self.images_left = [], []
+        self.index = 0
+        self.counter = 0
+
+        for i in range(1, 5):
+            right_img = pygame.image.load(f'img/guy{i}.png')
+            right_img = pygame.transform.scale(right_img, (40, 80))
+            left_img = pygame.transform.flip(right_img, True, False)
+            self.images_right.append(right_img)
+            self.images_left.append(left_img)
+
+        self.image = self.images_right[self.index]
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.vel_y = 0
+        self.jumped = False
+        self.dir_facing = 1
+
+    def update(self):
+        delta_x = 0
+        delta_y = 0
+        walk_cooldown = 5
+
+        # Get key presses
+        key = pygame.key.get_pressed()
+
+        if key[pygame.K_SPACE] and not self.jumped:
+            self.vel_y = -15
+            self.jumped = True
+        if not key[pygame.K_SPACE]:
+            self.jumped = False
+        if key[pygame.K_LEFT]:
+            delta_x -= 5
+            self.counter += 1
+            self.dir_facing = -1
+        if key[pygame.K_RIGHT]:
+            delta_x += 5
+            self.counter += 1
+            self.dir_facing = 1
+        if not key[pygame.K_RIGHT] and not key[pygame.K_LEFT]:
+            standing(self)
+
+        # Handling animations
+        if self.counter > walk_cooldown:
+            self.counter = 0
+            self.index += 1
+            if self.index >= len(self.images_right):
+                self.index = 0
+            self.image = self.images_right[self.index] if (self.dir_facing == 1) else self.images_left[self.index]
+
+        # Adding gravity
+        self.vel_y += 1
+        if self.vel_y > 10:
+            self.vel_y = 10
+        delta_y += self.vel_y
+
+        # Check for collisions
+        for tile in world.tile_list:
+            # Checking y-axis collision
+            if tile[1].colliderect(self.rect):
+
+
+        # Update the player's coordinates
+        self.rect.x += delta_x
+        self.rect.y += delta_y
+
+        if self.rect.bottom > screen_height:
+            self.rect.bottom = screen_height
+            delta_y = 0
+
+        # Draws player onto the screen
+        screen.blit(self.image, self.rect)
+
+
 world_data = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -79,14 +164,16 @@ world_data = [
 ]
 
 world = World(world_data)
+player = Player(100, screen_height - 130)
 
 run = True
 while run:
+    clock.tick(fps)
+
     screen.blit(bg_img, (0, 0))
-
-    draw_grid()
-
     world.draw()
+    player.update()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
